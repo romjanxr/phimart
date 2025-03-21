4a8a4759419afe181e19b80c7ad4ef5c40f3d7b1
Romjan Ali <romjanvr5@gmail.com>
2025-03-22 03:17:48 +0600

    updated initiate payment

--- order/views.py
+++ order/views.py
@@ -103,10 +103,13 @@ class OrderViewset(ModelViewSet):
 
 @api_view(['POST'])
 def initiate_payment(request):
+
+    print(request.data)
     user = request.user
     amount = request.data.get("amount")
     order_id = request.data.get("orderId")
     num_items = request.data.get("numItems")
+    print("User", user)
 
     settings = {'store_id': 'phima67ddc8dba290b',
                 'store_pass': 'phima67ddc8dba290b@ssl', 'issandbox': True}
@@ -125,7 +128,7 @@ def initiate_payment(request):
     post_body['cus_add1'] = user.address
     post_body['cus_city'] = "Dhaka"
     post_body['cus_country'] = "Bangladesh"
-    post_body['shipping_method'] = "Courier"
+    post_body['shipping_method'] = "NO"
     post_body['multi_card_name'] = ""
     post_body['num_of_item'] = num_items
     post_body['product_name'] = "E-commerce Products"
@@ -133,6 +136,7 @@ def initiate_payment(request):
     post_body['product_profile'] = "general"
 
     response = sslcz.createSession(post_body)  # API response
+    print(response)
 
     if response.get("status") == 'SUCCESS':
         return Response({"payment_url": response['GatewayPageURL']})
