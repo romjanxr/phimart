c3eccfaa57c788b791259092f62c5a08da7dccb6
Romjan Ali <romjanvr5@gmail.com>
2025-03-22 23:41:05 +0600

    sslcommerze api adde

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -211,6 +211,8 @@ EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
+BACKEND_URL = config("BACKEND_URL")
+FRONTEND_URL = config("FRONTEND_URL")
 
 """ 
 is_staff = false
