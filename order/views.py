e5f3a5910bd94f5a5132ff00b8aba95e2eaa2cc4
Romjan Ali <romjanvr5@gmail.com>
2025-04-07 23:30:52 +0600

    feat: Endpoint added for checking if a user has ordered the product or not

--- order/views.py
+++ order/views.py
@@ -13,6 +13,7 @@ from rest_framework.decorators import api_view
 from sslcommerz_lib import SSLCOMMERZ
 from django.conf import settings as main_settings
 from django.http import HttpResponseRedirect
+from rest_framework.views import APIView
 
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
@@ -161,3 +162,13 @@ def payment_cancel(request):
 def payment_fail(request):
     print("Inside fail")
     return HttpResponseRedirect(f"{main_settings.FRONTEND_URL}/dashboard/orders/")
+
+
+class HasOrderedProduct(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, product_id):
+        user = request.user
+        has_ordered = OrderItem.objects.filter(
+            order__user=user, product_id=product_id).exists()
+        return Response({"hasOrdered": has_ordered})
